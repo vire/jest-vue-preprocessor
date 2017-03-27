943f9be5e1968acd98089351b397a9a28c45c1de
Hajime Yamasaki Vukelic <hayavuk@gmail.com>
2017-03-27 22:14:41 +0200

    Add TypeScript support
    This patch adds support for compiling the body of the <script> tag which uses
    either lang="ts" or lang="typescript" using TypeScript compiler instead of
    Babel. Full backwards compatibility is maintained by making Babel a fallback
    compiler if the lang attrib is not one of the 'ts', 'typescript', or 'babel'.
    
    Locating and loading of the typescript configuration is deferred to tsconfig
    package.
    
    In order to unit-test the preprocessor, new fixture components are added which
    use the class-based component format. vue-property-decorator package is added as
    a development dependency to support this component.
    
    Signed-off-by: Hajime Yamasaki Vukelic <hayavuk@gmail.com>

---
+++ test/fixtures/TsComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="lorem-class">some test text</div>
+    <button v-on:click="clickHandler('value passed to clickHandler')">Click Me!</button>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue = require('vue')
+  import { Component } from 'vue-property-decorator'
+
+  @Component
+  export default class App extends Vue {
+    name = 'app'
+    clickHandler(input) {
+      return input + 1;
+    }
+  }
+</script>
+
+<style>
+  body {
+    color: red;
+  }
+</style>
+
