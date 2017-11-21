1df50d799f515d63a4ebe82e4df2c5e77da2e818
Kengo Hamasaki <k.hamasaki@gmail.com>
2017-11-21 01:30:49 -0800

    Show message from Babel (#45)
    * feat: show error message from Babel
    
    * fix: fix typo

--- transforms/transformBabel.js
+++ transforms/transformBabel.js
@@ -59,8 +59,10 @@ module.exports = function transformBabel(src, filename, render, staticRenderFns,
   try {
     result = babelCore.transform(src, combinedTransformOptions);
   } catch (error) {
-    // eslint-disable-next-line
-    console.error('Failed to compile scr with `babel` at `vue-preprocessor`');
+    /* eslint-disable no-console */
+    console.error('Failed to compile src with `babel` at `vue-preprocessor`');
+    console.error('Babel:', error);
+    /* eslint-enable */
   }
   return result;
 };
