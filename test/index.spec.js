c2679f793aa70add0b339d5d23a20cf5e777646b
Viliam Elischer <viliam.elischer@gmail.com>
2018-03-13 23:13:34 +0100

    Revert "Support for Functional Components (#50)"
    This reverts commit c0c059037b97c17431a2381ba6cd9d024daeace3.

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,7 +4,6 @@ import TsComponent from './fixtures/TsComponent.vue';
 import TypescriptComponent from './fixtures/TypescriptComponent.vue';
 import AbsolutePathComponent from 'test/fixtures/FooComponent.vue';
 import srcImportComponent from './fixtures/srcImportComponent/srcImportComponent.vue';
-import FunctionalComponent from 'test/fixtures/FunctionalComponent.vue';
 
 const doTest = Component => {
   const mockFn = jest.fn();
@@ -47,53 +46,5 @@ describe('preprocessor', () => {
 
   it('should process and parse a .vue component containing src referenecs', () => {
     doTest(srcImportComponent);
-  });
-
-  describe('when processing functional components', () => {
-    let vm;
-    let mockFn;
-
-    beforeEach(
-      (Component => () => {
-        mockFn = jest.fn();
-
-        vm = new Vue({
-          el: document.createElement('div'),
-          render: h =>
-            h(Component, {
-              context: {
-                props: {
-                  onClick: mockFn,
-                },
-              },
-            }),
-        });
-      })(FunctionalComponent)
-    );
-
-    it('doesn\'t throw errors for a nonexistent script tag', () => {
-      expect(vm._isVue).toEqual(true);
-    });
-
-    /*
-      This test serves purely to document that depending on
-      a machine's settings, this test can go either way
-     */
-    it('doesn\t have $el.querySelector', () => {
-      if (vm.$el.querySelector) {
-        expect(vm.$el.querySelector).toBeDefined();
-        expect(vm.$el.querySelector('.lorem-class').textContent).toEqual('some test text');
-
-        // check if template calls vue methods
-        vm.$el.querySelector('button').click();
-
-        // I can't get my local machine to branch into this
-        // group of statements, so I can't figure out what's
-        // going on here to make this fail.
-        // expect(mockFn.mock.calls[0][0]).toBe('value passed to clickHandler');
-      } else {
-        expect(vm.$el.querySelector).not.toBeDefined();
-      }
-    });
   });
 });
